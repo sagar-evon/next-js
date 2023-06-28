a61e47e4b29cea6e8e2e8ba14d170b5d02e64715
sagar-evon <sagar.sharma@evontech.com>
2023-06-28 16:11:19 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <section className='flex-start flex-col paddings mb-16'>
+      <h1>Categories</h1>
+      <h1>Posts</h1>
+      <h1>Load More</h1>
+
+    </section>
+  )
+}
